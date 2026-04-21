ccaa0ce8a22171c165c94c6d8aff2b2f550fd094
Aditya <adityavegulla@gmail.com>
2026-04-21 08:45:28 +0530

    Refactor input area and enhance user interaction features

--- docs/frontend/script.js
+++ docs/frontend/script.js
@@ -1,6 +1,10 @@
 const sidebarBtn = document.querySelector("#sidebarBtn");
 const sidebar = document.querySelector("#sidebar");
 const sidebarClo = document.querySelector("#sidebarClo");
+const userInput = document.querySelector("#userInput");
+const sendBtn = document.querySelector("#sendBtn");
+const remove1 = document.querySelector("#remove-1");
+const remove2 = document.querySelector("#remove-2");
 
 console.log("its working")
 sidebarBtn.addEventListener("click", () => {
@@ -12,4 +16,14 @@ sidebarClo.addEventListener("click", () => {
     sidebar.classList.remove("open")
     sidebarBtn.style.display = "flex";
 })
+
+userInput.addEventListener("input", ()=>{
+    userInput.style.height = "auto";
+    userInput.style.height = userInput.scrollHeight + "px";
+});
+
+sendBtn.addEventListener("click", ()=>{
+    remove1.remove();
+    remove2.remove();
+});
 
